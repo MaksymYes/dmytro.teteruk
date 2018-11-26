34bdd6105908194b49bc7df48e38c4da59b8fd53
Dmytro Teteruk <dmytro.teteruk@nure.ua>
2018-11-26 21:35:34 +0200

    Database exception class created

--- usermanagment/src/main/java/ua/nure/kn/teteruk/usermanagment/db/UserDao.java
+++ usermanagment/src/main/java/ua/nure/kn/teteruk/usermanagment/db/UserDao.java
@@ -1,12 +1,13 @@
 package ua.nure.kn.teteruk.usermanagment.db;
 
 import ua.nure.kn.teteruk.usermanagment.User;
+import ua.nure.kn.teteruk.usermanagment.db.exception.DatabaseException;
 
 import java.util.Collection;
 
 public interface UserDao {
 
-    User create(User user);
+    User create(User user) throws DatabaseException;
 
     void update(User user);
 
