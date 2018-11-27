4adcd4817be4e12c6d2a50fdfad0dc87c647127d
Dmytro Teteruk <dmytro.teteruk@nure.ua>
2018-11-27 23:30:57 +0200

    HsqldbUserDaoTest extends DatabaseTestCase

--- usermanagment/src/test/java/ua/nure/kn/teteruk/usermanagment/db/impl/HsqldbUserDaoTest.java
+++ usermanagment/src/test/java/ua/nure/kn/teteruk/usermanagment/db/impl/HsqldbUserDaoTest.java
@@ -1,5 +1,10 @@
 package ua.nure.kn.teteruk.usermanagment.db.impl;
 
+import org.dbunit.DatabaseTestCase;
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.XmlDataSet;
 import org.junit.Before;
 import org.junit.Test;
 import ua.nure.kn.teteruk.usermanagment.User;
@@ -7,13 +12,10 @@ import ua.nure.kn.teteruk.usermanagment.db.ConnectionFactory;
 import ua.nure.kn.teteruk.usermanagment.db.UserDao;
 import ua.nure.kn.teteruk.usermanagment.db.exception.DatabaseException;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import static junit.framework.TestCase.*;
-
-public class HsqldbUserDaoTest {
+public class HsqldbUserDaoTest extends DatabaseTestCase {
 
     private static final String FIRST_NAME = "Ivan";
     private static final String LAST_NAME = "Petrov";
@@ -22,12 +24,22 @@ public class HsqldbUserDaoTest {
     private ConnectionFactory factory;
     private UserDao dao;
 
+    @Override
+    protected IDatabaseConnection getConnection() throws Exception {
+        factory = new ConnecetionFactoryImpl();
+        return new DatabaseConnection(factory.createConnection());
+    }
+
+    @Override
+    protected IDataSet getDataSet() throws Exception {
+        return new XmlDataSet(getClass().getClassLoader().getResourceAsStream("usersDataSet.xml"));
+    }
+
     @Before
     public void setUp() throws Exception {
         Date birthDate = new SimpleDateFormat("d-MM-yyyy").parse("10-01-1988");
 
         user = new User(null, FIRST_NAME, LAST_NAME, birthDate);
-        factory = new ConnecetionFactoryImpl();
         dao = new HsqldbUserDao(factory);
     }
 
