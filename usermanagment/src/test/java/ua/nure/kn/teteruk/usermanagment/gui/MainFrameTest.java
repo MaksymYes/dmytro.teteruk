f885aa361e28bc363f0ea9514bd29853d66f70f1
Dmytro Teteruk <dmytro.teteruk@nure.ua>
2018-12-08 18:22:13 +0200

    Step 7. testAddUser method implemented

--- usermanagment/src/test/java/ua/nure/kn/teteruk/usermanagment/gui/MainFrameTest.java
+++ usermanagment/src/test/java/ua/nure/kn/teteruk/usermanagment/gui/MainFrameTest.java
@@ -2,6 +2,7 @@ package ua.nure.kn.teteruk.usermanagment.gui;
 
 import junit.extensions.jfcunit.JFCTestCase;
 import junit.extensions.jfcunit.JFCTestHelper;
+import junit.extensions.jfcunit.eventdata.MouseEventData;
 import junit.extensions.jfcunit.finder.NamedComponentFinder;
 import org.junit.After;
 import org.junit.Before;
@@ -11,7 +12,6 @@ import javax.swing.*;
 import java.awt.*;
 
 import static junit.extensions.jfcunit.TestHelper.cleanUp;
-import static org.junit.Assert.*;
 
 public class MainFrameTest extends JFCTestCase {
 
@@ -40,11 +40,24 @@ public class MainFrameTest extends JFCTestCase {
         find(JButton.class, "detailsButton");
     }
 
+    @Test
+    public void testAddUser() {
+        JButton addButton = (JButton) find(JButton.class, "addButton");
+        getHelper().enterClickAndLeave(new MouseEventData(this, addButton));
+
+        find(JPanel.class, "addPanel");
+        find(JTextField.class, "firstNameField");
+        find(JTextField.class, "lastNameField");
+        find(JTextField.class, "dateOfBirthField");
+        find(JButton.class, "okButton");
+        find(JButton.class, "cancelButton");
+    }
+
     private Component find(Class componentClass, String name) {
         NamedComponentFinder finder = new NamedComponentFinder(componentClass, name);
         finder.setWait(0);
         Component component = finder.find(mainFrame, 0);
-        assertNotNull("Could not find component \'" + name +"\'", component);
+        assertNotNull("Could not find component \'" + name + "\'", component);
         return component;
     }
 }
