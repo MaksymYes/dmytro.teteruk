2728f771d42970ec338a76c3b0d1062a303c4323
Dmytro Teteruk <dmytro.teteruk@nure.ua>
2018-12-08 17:41:12 +0200

    Step 5. MainFrame initialization and BrowsePanel are added.

---
+++ usermanagment/src/main/java/ua/nure/kn/teteruk/usermanagment/gui/BrowsePanel.java
@@ -0,0 +1,9 @@
+package ua.nure.kn.teteruk.usermanagment.gui;
+
+import javax.swing.*;
+
+public class BrowsePanel extends JPanel {
+
+    public BrowsePanel(MainFrame mainFrame) {
+    }
+}
--- usermanagment/src/main/java/ua/nure/kn/teteruk/usermanagment/gui/MainFrame.java
+++ usermanagment/src/main/java/ua/nure/kn/teteruk/usermanagment/gui/MainFrame.java
@@ -1,10 +1,50 @@
 package ua.nure.kn.teteruk.usermanagment.gui;
 
 import javax.swing.*;
+import java.awt.*;
+
+import static java.util.Objects.isNull;
 
 public class MainFrame extends JFrame {
 
-    public static void main(String[] args) {
+    private static final int FRAME_HEIGHT = 400;
+    private static final int FRAME_WIDTH = 600;
 
+    private JPanel contentPanel;
+
+    public JPanel getBrowsePanel() {
+        if (isNull(browsePanel)) {
+            browsePanel = new BrowsePanel(this);
+        }
+        return browsePanel;
     }
+
+    private JPanel browsePanel;
+
+    public MainFrame() {
+        super();
+        initialize();
+    }
+
+    private void initialize() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        this.setTitle("Управление пользователями");
+        this.setContentPane(getContentPanel());
+    }
+
+    private JPanel getContentPanel() {
+        if (isNull(contentPanel)) {
+            contentPanel = new JPanel();
+            contentPanel.setLayout(new BorderLayout());
+            contentPanel.add(getBrowsePanel(), BorderLayout.CENTER);
+        }
+        return contentPanel;
+    }
+
+    public static void main(String[] args) {
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.setVisible(true);
+    }
+
 }
