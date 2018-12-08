66b6b97c9b6817ca9357b9b90c747688735228a2
Dmytro Teteruk <dmytro.teteruk@nure.ua>
2018-12-08 17:21:37 +0200

    Step 3. Helper method find created

--- usermanagment/src/test/java/ua/nure/kn/teteruk/usermanagment/gui/MainFrameTest.java
+++ usermanagment/src/test/java/ua/nure/kn/teteruk/usermanagment/gui/MainFrameTest.java
@@ -2,8 +2,12 @@ package ua.nure.kn.teteruk.usermanagment.gui;
 
 import junit.extensions.jfcunit.JFCTestCase;
 import junit.extensions.jfcunit.JFCTestHelper;
+import junit.extensions.jfcunit.finder.NamedComponentFinder;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+
+import java.awt.*;
 
 import static junit.extensions.jfcunit.TestHelper.cleanUp;
 import static org.junit.Assert.*;
@@ -17,6 +21,13 @@ public class MainFrameTest extends JFCTestCase {
         setHelper(new JFCTestHelper());
         mainFrame = new MainFrame();
         mainFrame.setVisible(true);
+    }
+
+    private Component find(Class componentClass, String name) {
+        NamedComponentFinder finder = new NamedComponentFinder(componentClass, name);
+        Component component = finder.find(mainFrame, 0);
+        assertNotNull("Could not find component \'" + name +"\'", component);
+        return component;
     }
 
     @After
