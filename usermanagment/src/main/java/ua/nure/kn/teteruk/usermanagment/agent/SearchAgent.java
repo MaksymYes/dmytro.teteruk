ed7f9f1fc42b118ce4d62514195ddf449bd6054e
Dmytro Teteruk <dmytro_teteruk@epam.com>
2018-12-20 12:48:23 +0200

    Step 1. SearchAgent log implementation added. Set up run mode

---
+++ usermanagment/src/main/java/ua/nure/kn/teteruk/usermanagment/agent/SearchAgent.java
@@ -0,0 +1,17 @@
+package ua.nure.kn.teteruk.usermanagment.agent;
+
+import jade.core.Agent;
+
+public class SearchAgent extends Agent {
+    @Override
+    protected void setup() {
+        super.setup();
+        System.out.println(getAID().getName() + " started");
+    }
+
+    @Override
+    protected void takeDown() {
+        System.out.println(getAID().getName() + " terminated");
+        super.takeDown();
+    }
+}
