bfaab3287024389e8cf9a383e43ba6e83d057068
Dmytro Teteruk <dmytro_teteruk@epam.com>
2018-12-20 12:59:25 +0200

    Step 3. Add functionality to method search of SearchAgent class

--- usermanagment/src/main/java/ua/nure/kn/teteruk/usermanagment/agent/SearchAgent.java
+++ usermanagment/src/main/java/ua/nure/kn/teteruk/usermanagment/agent/SearchAgent.java
@@ -1,6 +1,13 @@
 package ua.nure.kn.teteruk.usermanagment.agent;
 
+import java.util.Collection;
+
+import org.dbunit.util.search.SearchException;
+
 import jade.core.Agent;
+import ua.nure.kn.teteruk.usermanagment.User;
+import ua.nure.kn.teteruk.usermanagment.db.DAOFactory;
+import ua.nure.kn.teteruk.usermanagment.db.exception.DatabaseException;
 
 public class SearchAgent extends Agent {
     @Override
@@ -13,5 +20,22 @@ public class SearchAgent extends Agent {
     protected void takeDown() {
         System.out.println(getAID().getName() + " terminated");
         super.takeDown();
+    }
+
+    public void search(String firstName, String lastName) throws SearchException {
+        try {
+            Collection<User> users = DAOFactory.getInstance().getUserDao().find(firstName, lastName);
+            if (!users.isEmpty()) {
+                showUsers(users);
+            } else {
+                // todo request to other
+            }
+        } catch (DatabaseException e) {
+            throw new SearchException(e);
+        }
+    }
+
+    private void showUsers(Collection<User> users) {
+        // todo implement method
     }
 }
