cefa7e399a81f0f912c0a4afa0b2107d2cd618ff
Dmytro Teteruk <dmytro_teteruk@epam.com>
2018-12-20 13:40:44 +0200

    Step 9. SearchAgent added TickerBehavior

--- usermanagment/src/main/java/ua/nure/kn/teteruk/usermanagment/agent/SearchAgent.java
+++ usermanagment/src/main/java/ua/nure/kn/teteruk/usermanagment/agent/SearchAgent.java
@@ -6,6 +6,7 @@ import org.dbunit.util.search.SearchException;
 
 import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.TickerBehaviour;
 import jade.domain.DFService;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
@@ -15,6 +16,9 @@ import ua.nure.kn.teteruk.usermanagment.db.DAOFactory;
 import ua.nure.kn.teteruk.usermanagment.db.exception.DatabaseException;
 
 public class SearchAgent extends Agent {
+
+    private AID[] aids;
+
     @Override
     protected void setup() {
         super.setup();
@@ -32,6 +36,26 @@ public class SearchAgent extends Agent {
             e.printStackTrace();
         }
 
+        addBehaviour(new TickerBehaviour(this, 60000) {
+
+            @Override
+            protected void onTick() {
+                DFAgentDescription agentDescription = new DFAgentDescription();
+                ServiceDescription serviceDescription1 = new ServiceDescription();
+                serviceDescription1.setType("searching");
+                agentDescription.addServices(serviceDescription1);
+                try {
+                    DFAgentDescription[] descriptions = DFService.search(myAgent, agentDescription);
+                    aids = new AID[descriptions.length];
+                    for (int i = 0; i < descriptions.length; i++) {
+                        DFAgentDescription d = descriptions[i];
+                        aids[i] = d.getName();
+                    }
+                } catch (FIPAException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
         addBehaviour(new RequestServer());
     }
 
@@ -52,7 +76,7 @@ public class SearchAgent extends Agent {
             if (!users.isEmpty()) {
                 showUsers(users);
             } else {
-                addBehaviour(new SearchRequestBehaviour(new AID[] {}, firstName, lastName));
+                addBehaviour(new SearchRequestBehaviour(aids, firstName, lastName));
             }
         } catch (DatabaseException e) {
             throw new SearchException(e);
