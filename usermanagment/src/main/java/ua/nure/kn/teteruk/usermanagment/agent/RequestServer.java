14065bd8749a0b9b431f2d527b0256dd7ac9a503
Dmytro Teteruk <dmytro_teteruk@epam.com>
2018-12-20 13:26:44 +0200

    Step 6. RequestServer reply method implemented

--- usermanagment/src/main/java/ua/nure/kn/teteruk/usermanagment/agent/RequestServer.java
+++ usermanagment/src/main/java/ua/nure/kn/teteruk/usermanagment/agent/RequestServer.java
@@ -1,11 +1,16 @@
 package ua.nure.kn.teteruk.usermanagment.agent;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.Objects;
+import java.util.StringTokenizer;
 
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
 import ua.nure.kn.teteruk.usermanagment.User;
+import ua.nure.kn.teteruk.usermanagment.db.DAOFactory;
+import ua.nure.kn.teteruk.usermanagment.db.exception.DatabaseException;
 
 public class RequestServer extends CyclicBehaviour {
 
@@ -29,6 +34,29 @@ public class RequestServer extends CyclicBehaviour {
     }
 
     private ACLMessage createReply(ACLMessage message) {
-        return null;
+        ACLMessage reply = message.createReply();
+
+        String contet = message.getContent();
+        StringTokenizer tokenizer = new StringTokenizer(contet, ",");
+        if (tokenizer.countTokens() == 2) {
+            String firstName = tokenizer.nextToken();
+            String lastName = tokenizer.nextToken();
+            Collection<User> users = null;
+            try {
+                users = DAOFactory.getInstance().getUserDao().find(firstName, lastName);
+            } catch (DatabaseException e) {
+                e.printStackTrace();
+                users = Collections.emptyList();
+            }
+            StringBuffer buffer = new StringBuffer();
+            for (Iterator it = users.iterator(); it.hasNext(); ) {
+                User user = (User) it.next();
+                buffer.append(user.getId()).append(",")
+                        .append(user.getFirstName()).append(",")
+                        .append(user.getLastName()).append(";");
+            }
+            reply.setContent(buffer.toString());
+        }
+        return reply;
     }
 }
